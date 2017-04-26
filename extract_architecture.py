f162a2f251aea010406bf9dba224b37bee3c4ee9
KaiserKlayton <claytonvioland@gmail.com>
2017-04-26 21:05:29 +0200

    Added Python function to extract model architectures and parameters from .prototxt files and write them to a (ordered) Python dictionary; Added functionality to recursively extract Caffe model features

---
+++ extract_architecture.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+__author__ = "C. Clayton Violand"
+__copyright__ = "Copyright 2017"
+
+## Function that returns model architecture and parameters as a (ordered) Python dictionary.
+## REQUIRED: Caffe .prototxt located in: 'models/<model_name>/..'.
+##
+
+import os
+import re
+import sys
+
+from collections import OrderedDict
+
+def extract_architecture(d):
+    layer_types = ['conv', 'pool', 'relu', 'ip', 'fc']
+    param_types = ['num_output', 'pad', 'kernel_size', 'stride']
+    special_types = ['shape', 'input_dim']
+    shape_dims = ['n','d','w','h']
+
+    architecture = OrderedDict()
+    architecture['shape'] = {}
+
+    prototxt_file = open(d,'r').readlines()
+    for l in prototxt_file:
+        for s in special_types:
+            if s in l:
+                if s == 'shape':
+                    param_match = re.search(s + ': { dim: ([0-9]+) dim: ([0-9]+) dim: ([0-9]+) dim: ([0-9]+)', l)
+                    if param_match:
+                        architecture['shape']['n'] = int(param_match.group(1))
+                        architecture['shape']['d'] = int(param_match.group(2))
+                        architecture['shape']['w'] = int(param_match.group(3))
+                        architecture['shape']['h'] = int(param_match.group(4))                           
+                elif s == 'input_dim':
+                    param_match = re.search(s + ': ([0-9]+)', l)
+                    if param_match:
+                        architecture['shape'][shape_dims.pop(0)] = param_match.group(1)
+                else:
+                    sys.exit("Unknown shape format")
+
+        layer_match = re.search('name: "(([a-z]+)[0-9]+)"', l)
+        if layer_match:
+            if layer_match.group(2) not in layer_types:
+                sys.exit("Unknown layer type: %s") % layer_match.group(2)
+            layer = layer_match.group(1)
+            architecture[layer] = {}
+          
+        for p in param_types:
+            if p in l:
+                param_match = re.search(p + ': ([0-9]+)', l)
+                if param_match:
+                    value = int(param_match.group(1))
+                    architecture[layer][p] = value
+
+    # Deal with pooling layers that aren't kernel_size=2
+    for key in architecture:
+        if "pool" in key:
+            for j in architecture[key]:
+                if j == "kernel_size":
+                    if architecture[key][j] != 2:
+                        sys.exit("pooling layer with filter size != 2. Edit in .prototxt and try again.")
+
+    return architecture
+
+if __name__ == "__main__":
+    extract_architecture("")
---
+++ extract_caffe_features.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+__author__ = "C. Clayton Violand"
+__copyright__ = "Copyright 2017"
+
+## Extracts Caffe features from Caffe models. Writes to file at: 'features/<model_name>/caffe/..'.
+## REQUIRED: Caffe .prototxt and .caffemodel in: 'models/<model_name>/..'
+##           Input data in: 'inputs/<model_name>/..'
+##
+
+import os
+import re
+import sys
+
+import numpy as np
+import caffe
+import cPickle
+
+from extract_architecture import extract_architecture
+
+caffe.set_mode_cpu()
+
+def unpickle(file):
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo)
+    fo.close()
+
+    return dict
+
+def main():    
+    dirs = [x[0] for x in os.walk('models/')]
+
+    for d in dirs:
+        model_match = re.search("models/(.+)", d)
+        if model_match:
+            model = model_match.group(1)
+        else: 
+            continue
+
+        # Get input data path.
+        input_file = os.listdir("inputs/" + model + "/production/")
+        input_file_path = "inputs/" + model + "/production/" + input_file[0]
+        try: 
+            input_file_path
+        except:
+            sys.exit("Error: No input data found...")                
+       
+        # Get .prototxt and .caffemodel path.
+        for f in os.listdir(d):
+            if f.endswith('.prototxt'):
+                prototxt_file_path = os.path.join(d, f)
+            if f.endswith('.caffemodel'):
+                model_file_path = os.path.join(d, f) 
+        try: 
+            prototxt_file_path
+        except:
+            sys.exit("Error: No suitable Caffe .prototxt found...")    
+        try: 
+            model_file_path
+        except:
+            sys.exit("Error: No suitable .caffemodel file found...")    
+
+        # Extract architecture and parameters.
+        architecture = extract_architecture(prototxt_file_path)
+        a = architecture
+
+        # Define caffe net.
+        net = caffe.Net(prototxt_file_path, model_file_path, caffe.TEST)
+
+        # Load (1) image into data blob.
+        if input_file_path.endswith('.binaryporoto'):
+            blob = caffe.proto.caffe_pb2.BlobProto()
+            data = open(input_file_path, 'rb').read()
+            blob.ParseFromString(data)
+            arr = np.array(caffe.io.blobproto_to_array(blob))
+            image = arr[0]
+            net.blobs['data'].data[...] = image
+        elif input_file_path.endswith('.csv'):
+            images = np.loadtxt(input_file_path, delimiter=",")
+            image = images[0,:]        
+            image = images[0,1:(a['shape']['d'] * a['shape']['w'] * a['shape']['h'])+1]    
+            image =  image.reshape(1,a['shape']['d'], a['shape']['w'], a['shape']['h'])
+            net.blobs['data'].data[...] = image
+        elif input_file_path.endswith('.pkl'):
+            images = unpickle(input_file_path)
+            images = images['data']
+            image =  images[0,:].transpose()
+            image = image.reshape(1,a['shape']['d'] * a['shape']['w'] * a['shape']['h'])
+            image = image.reshape(1,a['shape']['d'], a['shape']['w'], a['shape']['h'])
+            net.blobs['data'].data[...] = image
+  
+        # Forward propogate (1) image.
+        out = net.forward()
+        
+        # Extract and write features for each relevant layer.
+        for key in architecture:
+            if key == "shape" or "relu" in key:
+                continue
+   
+            blob = net.blobs[key].data[...]
+   
+            if len(blob.shape) == 4:
+                blob = blob.reshape(blob.shape[0]*blob.shape[1], blob.shape[2]*blob.shape[3]) 
+            elif len(blob.shape) == 3:
+                blob = blob.reshape(blob.shape[0], blob.shape[1], blob.shape[2])
+            else:
+                pass
+            
+            np.savetxt(os.path.join('features', model, "caffe", key+".csv"), blob, fmt='%f', delimiter=',')
+
+if __name__ == "__main__":
+    main()
